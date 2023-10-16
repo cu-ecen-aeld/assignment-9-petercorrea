53379cc3cc796908f06a7b478ce248996f28a110
Pedro Correa <pmcorrea@me.com>
2023-10-16 05:55:14 +0000

    assignment-7 part 2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -14,12 +14,12 @@ LDD_GIT_SUBMODULES = YES
 
 LDD_MODULE_SUBDIRS = scull misc-modules
 
-# define LDD_INSTALL_TARGET_CMDS
-# 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-# 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-# 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-# 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-# endef
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
